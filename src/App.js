12ed980b6b8c2bdd3c4c8eaa12d6920b72c23406
ravi barla <ravibarla1@gmail.com>
2023-08-31 22:59:44 +0530

    adding initial components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import DetailView from "./comonents/DetailView";
+// import Heading from "./comonents/Heading";
+import WeekView from "./comonents/weekView";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="detailView" element={<DetailView />} />
+          <Route path="weekView" element={<WeekView />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
